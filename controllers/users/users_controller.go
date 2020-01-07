63ee182acb7d4ad19aca4b7bdb3d9294878219ba
Muneer <muneerpp@bizbuzzz.com>
2020-01-07 13:19:54 +0530

    added marshalling

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,15 +1,29 @@
 package users
 
 import (
+	"bookstore_users-api/domain/users"
+	"bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func GetUser(c *gin.Context){
-	c.String(http.StatusNotImplemented,"not implemented")
-}
+
 
 func CreateUser(c *gin.Context){
+	var user users.User
+	if err :=  c.ShouldBindJSON(&user); err != nil {
+		// TODO Error handler here
+	}
+	result , saveError := services.CreateUser(user)
+	if saveError != nil {
+		// Handle User Creation Error
+		return
+	}
+
+	c.JSON(http.StatusCreated,result)
+}
+
+func GetUser(c *gin.Context){
 	c.String(http.StatusNotImplemented,"not implemented")
 }
 
--- services/users_service.go
+++ services/users_service.go
@@ -1,2 +1,8 @@
+// will be responsible for dealing with database
 package services
 
+import "bookstore_users-api/domain/users"
+
+func CreateUser(user users.User) (*users.User , error) {
+	return &user, nil
+}
